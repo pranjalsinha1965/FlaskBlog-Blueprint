66c2702a9d113e22613889fa9450f40a3d874b7c
pranjalsinha1965 <pranjalsinha1965@gmail.com>
2025-11-28 23:50:24 +0530

    Add registration form

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for 
+from forms import RegistrationForm, LoginForm 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '7fdef6f33454ed00a6b93d7463cdb9aa'
 
 posts = [
     {
@@ -24,6 +27,16 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('register.html', title='Register', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
